cfa5de0b5b037e2e051e450a50b73dd2060cfe3f
niushisan <78002887+niushisan@users.noreply.github.com>
2021-03-17 10:57:22 +0800

    更新！

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ export PATH=$CIHOME:$PATH
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
-sudo apt install bash wget curl unzip zip tar busybox vim nano git python qemu nodejs python3 miredo miredo-server ntpdate -y
+sudo apt install bash wget curl unzip zip tar busybox vim nano git python qemu-system mingw-w64 nodejs python3 miredo miredo-server ntpdate -y
 sudo ntpdate pool.ntp.org
 wget https://dl.google.com/android/repository/android-ndk-r21b-linux-x86_64.zip -O $CIHOME/ndk.zip
 unzip $CIHOME/ndk.zip
@@ -17,7 +17,7 @@ $NDKHOME/build/tools/make-standalone-toolchain.sh --install-dir=$CIHOME/android/
 $NDKHOME/build/tools/make-standalone-toolchain.sh --install-dir=$CIHOME/android/x86 --arch=x86 --platform=android-28
 $NDKHOME/build/tools/make-standalone-toolchain.sh --install-dir=$CIHOME/android/x86_64 --arch=x86_64 --platform=android-28
 export PATH=$NDKHOME:$CIHOME/android/arm/bin:$CIHOME/android/arm64/bin:$CIHOME/android/x86/bin:$CIHOME/android/x86_64/bin:$PATH
-wget https://golang.google.cn/dl/go1.15.7.linux-amd64.tar.gz -O $CIHOME/go.tar.gz
+wget https://golang.org/dl/go1.16.2.linux-amd64.tar.gz -O $CIHOME/go.tar.gz
 tar -zxf $CIHOME/go.tar.gz
 rm -rf $CIHOME/go.tar.gz
 export GOROOT=$CIHOME/go
